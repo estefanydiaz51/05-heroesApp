363a2b5f238839e995b79440a53066cc26db6487
estefanydiaz51 <yuranimetal@gmail.com>
2022-04-13 18:44:56 -0500

    heroesapp update

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 6px;
+    }
+  `
   ]
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe!: Heroe;
+
+  constructor( private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    /*
+    this.activatedRoute.params
+      .subscribe( ( {id} ) => {
+        this.heroesService.getHeroePorId( id ).subscribe(
+          (heroe ) => this.heroe = heroe,
+        )
+      }
+    ) */
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.heroesService.getHeroePorId(id))
+    ).subscribe( heroe => this.heroe = heroe)
+  }
+
+
+  regresar () {
+    this.router.navigate(['/heroes/listado'])
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule  } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
